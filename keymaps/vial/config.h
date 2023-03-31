79dddf0cf770edbf0700b00f28c1bc145d7bae37
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2023-03-31 22:07:27 +0800

    add vial support

---
+++ keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 rinmichi (@rinmichi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID { 0x3E, 0xCF, 0x11, 0x94, 0x78, 0x25, 0xDE, 0x4E }
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
---
+++ keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+# This file intentionally left blank
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+LTO_ENABLE = yes
