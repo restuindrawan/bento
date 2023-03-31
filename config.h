5d45604c44e912a70fe6080e18c0651cfce05612
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2023-03-31 22:03:38 +0800

    cleanup rgb feature

--- config.h
+++ config.h
@@ -11,53 +11,32 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+SPDX - License - Identifier : GPL - 2.0 - or -later
 */
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xCB10
-#define PRODUCT_ID      0x1133
-#define DEVICE_VER      0x0100
-#define MANUFACTURER    nguyedt
-#define PRODUCT         Bento
-#define DESCRIPTION     2x3 Macropad with Rotary Encoder
+/* QMK DFU */
+#define QMK_ESC_OUTPUT F1 // usually COL
+#define QMK_ESC_INPUT D5  // usually ROW
 
 /* key matrix size */
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 3
 
 /* Keyboard Matrix Assignments */
-#define DIRECT_PINS { \
-    { D2, D4, F4 }, \
-    { D7, B1, B3 } \
-}
+#define DIRECT_PINS                                                            \
+  {                                                                            \
+    {D2, D4, F4}, { D7, B1, B3 }                                               \
+  }
 
-/* Handwired Pin Layout:
-
-    { D7, B1, D2 }, \
-    { E6, B4, B2 } \
-*/
-
-#define ENCODERS_PAD_A {D1}
-#define ENCODERS_PAD_B {F5}
-
-#define BACKLIGHT_PIN B6
-#define BACKLIGHT_LEVELS 7
-
-#define RGB_DI_PIN D3
-#ifdef RGB_DI_PIN
-  #define RGBLED_NUM 11
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-  #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-  /*== all animations enable ==*/
-  #define RGBLIGHT_ANIMATIONS
-#endif
+/* Encoders Basic (EC11 compatible) encoders */
+#define ENCODERS_PAD_A                                                         \
+  { D1 }
+#define ENCODERS_PAD_B                                                         \
+  { F5 }
+#define ENCODER_DIRECTION_FLIP
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
