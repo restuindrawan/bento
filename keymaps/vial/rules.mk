5140bbbeb8f4b2c5d16c5c164e915fe1cf5d482a
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2023-04-01 11:43:02 +0800

    disable qmk feature

--- keymaps/vial/rules.mk
+++ keymaps/vial/rules.mk
@@ -1,7 +1,12 @@
-# This file intentionally left blank
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+
 LTO_ENABLE = yes
+
+QMK_SETTINGS = no
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
